416d7cf3c432563c3e8fabbaab44e42662cce426
GOHAN-P <pugachov.g@gmail.com>
2016-05-07 16:59:59 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '8e316fb977c237ffdf11dd0a3dca6f6ab3ee9271ceb83488aa57fddfa6c9c2e466f6e7d0f4221e5c0122cf7e1ee52fdc0ee4b28266184bcffeaf64f0e73e9deb'
---
+++ s
@@ -0,0 +1,10 @@
+                                  Список баз данных
+    Имя    | Владелец | Кодировка | LC_COLLATE  |  LC_CTYPE   |     Права доступа     
+-----------+----------+-----------+-------------+-------------+-----------------------
+ postgres  | postgres | UTF8      | ru_RU.UTF-8 | ru_RU.UTF-8 | 
+ template0 | postgres | UTF8      | ru_RU.UTF-8 | ru_RU.UTF-8 | =c/postgres          +
+           |          |           |             |             | postgres=CTc/postgres
+ template1 | postgres | UTF8      | ru_RU.UTF-8 | ru_RU.UTF-8 | =c/postgres          +
+           |          |           |             |             | postgres=CTc/postgres
+(3 строки)
+
